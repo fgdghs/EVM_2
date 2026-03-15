19848a2526e7e4f59a7b90f957187d06dc59fab4
fgdghs <golovachev.ke@gmail.com>
2026-03-15 16:13:24 +1000

    123123123123

--- Class_work.py
+++ Class_work.py
@@ -1,4 +1,3 @@
-
 # arr = [1,2,2,4]
 
 
@@ -7,6 +6,7 @@
 #     print("True")
 # else:
 #     print("False")
+
 
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
@@ -17,7 +17,7 @@ class Solution:
         while right < len(prices):
             if prices[left] < prices[right]:
                 profit = prices[right] - prices[left]
-                max_profit = max(max_profit,profit)
+                max_profit = max(max_profit, profit)
             else:
                 left = right
             right += 1
@@ -27,8 +27,8 @@ class Solution:
     def findMaxAverage(self, nums: list[int], k: int) -> float:
         cur_sum = sum(nums[:k])
         max_sum = cur_sum
-        
-        for i in range(k,len(nums)):
+
+        for i in range(k, len(nums)):
 
             cur_sum = cur_sum + nums[i]
             cur_sum = cur_sum - nums[i - k]
@@ -37,5 +37,3 @@ class Solution:
                 max_sum = cur_sum
 
         return max_sum / k
-    
-
